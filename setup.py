5addf508307315e6d56bed25da4ff32f3291e69b
jiahui-lim <jiahui@friartuck.net>
2022-02-22 16:37:43 +0800

    new repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='H&M Personalized Fashion Recommendation Competition',
+    author='jiahui',
+    license='MIT',
+)
